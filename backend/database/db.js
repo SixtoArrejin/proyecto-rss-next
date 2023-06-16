7744e5a7947b366a7afa0a2f8b0ff2d244805c40
SixtoArrejin <128509377+SixtoArrejin@users.noreply.github.com>
2023-06-16 10:28:39 -0300

    mi commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("Seleccion", "root", "", {
+const db = new Sequelize("Seleccion", "root", "Arrejin_468", {
   host: "localhost",
   dialect: "mysql",
 });
