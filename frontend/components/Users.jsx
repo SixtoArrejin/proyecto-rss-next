4ec92fddb753720dbea89ed0a907524d222271cf
Tobias Maciel <tobiasmaciel03@gmail.com>
2023-06-16 01:40:38 -0300

    Ultra funcional

--- frontend/components/Users.jsx
+++ frontend/components/Users.jsx
@@ -1,28 +1,55 @@
 "use client";
 import { useRouter } from "next/navigation";
+import './User.css'
 
 function Users({ jugadores }) {
   const router = useRouter();
 
   return (
     <div>
-      <ul className="list-group">
-        {jugadores.map((jugador) => (
+      {/* <ul className="list-group">
+        {users.map((user) => (
           <li
             className="list-group-item d-flex justify-content-between align-items-center list-group-item-action"
-            key={jugador.id}
-            onClick={() => router.push(`/users/${jugador.id}`)}
+            key={user.id}
+            onClick={() => router.push(`/users/${user.id}`)}
           >
             <div>
               <h5>
-                {jugador.id}. {jugador.nombre_apellido} {jugador.numero_camiseta}
+                {user.id}. {user.first_name} {user.last_name}
               </h5>
-              <p>Altura: {jugador.altura} Posicion: {jugador.posicion}</p>
+              <p>Email: {user.email}</p>
             </div>
-            <img src={jugador.imagen} style={{ borderRadius: "50%" }} />
+            <img src={user.avatar} style={{ borderRadius: "50%" }} />
           </li>
         ))}
-      </ul>
+      </ul> */}
+
+      <div className="user-cards-container">
+        {jugadores.map((jugador, index) => (
+          <div className="card" style={{ width: "18rem", cursor: "pointer" }} key={index} onClick={() => router.push(`/users/${jugador.id}`)}>
+              <img src={jugador.imagen} className="card-img-top" alt="La escaloneta" />
+              <div className="card-body">
+                <h5 className="card-title">{jugador.nombre_apellido}</h5>
+                <p className="card-text">
+                  Juega de {jugador.posicion}
+                </p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Camiseta: </li>
+                <li className="list-group-item">Altura: </li>
+                <li className="list-group-item">Edad: </li>
+                <li className="list-group-item">Goles: </li>
+              </ul>
+              {/* <div className="card-body">
+                <a href="#" className="card-link">
+                  Another link
+                </a>
+              </div> */}
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
